9628a286db51316c856a86f5bdf877331224e834
Don Gill <dongill@microsoft.com>
2016-09-28 11:47:00 -0700

    implemented the database bits

--- learning/web/flask/microblog/app/views.py
+++ learning/web/flask/microblog/app/views.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, flash, redirect
 from app import app
 from .forms import LoginForm
 
@@ -45,6 +45,7 @@ def login():
         flash('Login requested for OpenID="%s", remember_me=%s' %
               (form.openid.data, str(form.remember_me.data)))
         return redirect('/index')
-    return render_template('login.html',
+    return render_template('login.html', 
                            title='Sign In',
-                           form=form)
+                           form=form,
+                           providers=app.config['OPENID_PROVIDERS'])
